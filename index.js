88073ec26a38d4f3f67b606721caf69d67bce3be
Juan Ignacio Benito <benitojuanignacio@gmail.com>
2020-04-05 03:39:02 +0100

    Added { useUnifiedTopology: true } to mongoose

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const mongoose = require('mongoose')
 
 mongoose.set("useCreateIndex", true)
 
-mongoose.connect(MONGO_URI, { useNewUrlParser: true, useFindAndModify: false })
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true })
     .then(() => {
         server.start()
     })
